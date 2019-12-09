0bff2d82bf3f14475e0ec7ff7ad3cedba8b687f1
Sokol Makolli <st116214@stud.uni-stuttgart.de>
2019-12-09 10:24:51 +0100

    init commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <chrono>
+#include "vendor/path_finder/include/CHGraph.h"
+#include "vendor/path_finder/include/GraphReader.h"
+#include "vendor/path_finder/include/CHDijkstra.h"
+#include "vendor/path_finder/include/HubLabels.h"
+
+int main(int argc, char* argv[]) {
+    std::string filepath;
+    for(int i = 1; i < argc; ++i) {
+        std::string option = argv[i];
+        if(option == "-f") {
+            filepath = argv[++i];
+        }
+    }
+    std::cout << filepath << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    pathFinder::CHGraph chGraph;
+    pathFinder::GraphReader::readCHFmiFile(chGraph, filepath);
+    pathFinder::HubLabels hl(chGraph);
+    while(true) {
+        std::cout << "source: ";
+        pathFinder::NodeId source;
+        std::cin >> source;
+        pathFinder::NodeId target;
+        std::cout << "target: ";
+        std::cin >> target;
+        auto start = std::chrono::high_resolution_clock::now();
+        std::cout << "Distance: " <<  hl.getShortestDistance(source, target).value() << std::endl;
+        auto finish = std::chrono::high_resolution_clock::now();
+        auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(finish - start);
+        std::cout << "Elapsed time: " << elapsed.count() << " µs\n";
+    }
+
+    return 0;
+}
