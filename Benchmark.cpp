b43880692d5918aaaa5476dc0a99a172c578a8f8
Sokol Makolli <st116214@stud.uni-stuttgart.de>
2020-04-24 17:00:48 +0200

    add gridReorder cmd option

--- Benchmark.cpp
+++ Benchmark.cpp
@@ -5,18 +5,20 @@
 int main(int argc, char* argv[]) {
   std::string filepath;
   int level = 0;
-
+  bool gridReorder = false;
   for(int i = 1; i < argc; ++i) {
     std::string option = argv[i];
     if(option == "-f")
       filepath = argv[++i];
     else if(option == "-l")
       level = std::stoi(argv[++i]);
+    else if(option == "-gridReorder")
+      gridReorder = true;
   }
 
   std::cout << filepath << std::endl;
   pathFinder::CHGraph chGraph;
-  pathFinder::GraphReader::readCHFmiFile(chGraph, filepath);
+  pathFinder::GraphReader::readCHFmiFile(chGraph, filepath, gridReorder);
   pathFinder::ChHlBenchmarker bm(chGraph);
   bm.compareSpeed("hl-ram.bench", level);
   return 0;
